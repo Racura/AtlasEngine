e0f86e84ea838c407393a3818c44836ce4d5838e
Racura <hellsuggestionbox@gmail.com>
2013-10-29 16:46:44 +1100

    changes to how color works

--- AtlasColorSystem.cs
+++ AtlasColorSystem.cs
@@ -10,30 +10,36 @@ namespace AtlasEngine
 {
     public class AtlasColorSystem
     {
-        public static Color GetColorFromHue(float hue, float brightness, float saturation)
+        public static Color RGBFromHSL(float hue, float saturation, float light)
         {
-            return new Color(GetValueFromHue(hue) * saturation      + brightness,
-                            GetValueFromHue(hue + 120) * saturation + brightness,
-                            GetValueFromHue(hue + 240) * saturation + brightness);
-        }
+            if (saturation <= 0)
+                return new Color(light, light, light);
 
-        public static Color GetColorFromHue(float hue)
-        {
-            return new Color(GetValueFromHue(hue),
-                            GetValueFromHue(hue + 120),
-                            GetValueFromHue(hue + 240));
-        }
+            hue = ((hue + (hue < 0 ? 6 : 0)) % 360) / 60;
 
-        private static float GetValueFromHue(float degree)
-        {
-            degree = degree % 360;
-            if (degree < 0) degree += 360;
+            if (hue < 0)
+                hue = hue + 6;
 
-            if (degree < 60)  return 1;
-            if (degree < 120) return (120 - degree) / 60f;
-            if (degree < 240) return 0;
-            if (degree < 300) return (degree - 240) / 60f;
-            return 1;
+            float c = (1 - Math.Abs(light * 2 - 1)) * saturation;
+            float x = c * (1 - Math.Abs(hue % 2 - 1));
+            float m = light - c * 0.5f;
+
+            switch ((int)hue)
+            {
+                case 0:
+                    return new Color(m + c, m + x, m);
+                case 1:
+                    return new Color(m + x, m + c, m);
+                case 2:
+                    return new Color(m, m + c, m + x);
+                case 3:
+                    return new Color(m, m + x, m + c);
+                case 4:
+                    return new Color(m + x, m, m + c);
+                case 5:
+                default:
+                    return new Color(m + c, m, m + x);
+            }
         }
     }
 }
